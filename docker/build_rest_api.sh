6c54f88bc7e6f2488384496a0d2762a57f2cf63c
phanimaridu <phani.vjec@gmail.com>
2017-03-15 21:02:03 -0700

    Removed test skip from docker build script

--- docker/build_rest_api.sh
+++ docker/build_rest_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# TODO:Error Handling 
+# TODO:Error Handling
 
 #Init variables
 REST_API_JAR_PATH=../rest-api/build/libs/openams-rest-api-0.0.1-SNAPSHOT.jar
@@ -8,19 +8,17 @@ IMAGE_NAME=phanimaridu/openams-rest-api:0.0.1-SNAPSHOT
 
 # clean and build rest api spring boot project
 echo 'Initiating rest-api gradle build ...'
-gradle -b ../rest-api/build.gradle clean build -x test
+gradle -b ../rest-api/build.gradle clean build 
 echo 'Completed rest-api gradle build.'
 
 # copy built jar to docker directroy
 cp $REST_API_JAR_PATH ./rest-api/app.jar
 echo 'Copied Spring Boot App Jar to Docker directory'
-#cp ../rest-api/build/libs/openams-rest-api-0.0.1-SNAPSHOT.jar ./rest-api/app.jar
 
 # build docker image
 echo 'Initiating rest-api docker build ...'
 docker build -t $IMAGE_NAME rest-api
 echo 'Completed rest-api docker build.'
-#docker build -t phanimaridu/openams-rest-api:0.0.1-SNAPSHOT rest-api
 
 # delete app.jar
 rm ./rest-api/app.jar
