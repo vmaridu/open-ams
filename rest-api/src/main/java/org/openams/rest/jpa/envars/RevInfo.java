9a86f53a4a20cba760355f7a8c44b3074446cf7f
phanimaridu <phani.vjec@gmail.com>
2017-08-25 07:35:27 -0700

    Fixed SEQUENCE Compatibility issue for MySQL

--- rest-api/src/main/java/org/openams/rest/jpa/envars/RevInfo.java
+++ rest-api/src/main/java/org/openams/rest/jpa/envars/RevInfo.java
@@ -3,9 +3,7 @@ package org.openams.rest.jpa.envars;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import org.hibernate.envers.RevisionEntity;
@@ -21,8 +19,9 @@ import lombok.Data;
 public class RevInfo {
 
 	@Id
-	@SequenceGenerator(name = "SEQ_GEN", sequenceName = "REVINFO_SEQ", allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_GEN")
+	@GeneratedValue
+	//@SequenceGenerator(name = "SEQ_GEN", sequenceName = "REVINFO_SEQ", allocationSize = 1)
+	//@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_GEN")
 	@Column(name = "REV")
 	@RevisionNumber
 	private long id;
